dd7c1026d4120b88686f7dd102939cd7bec063df
Masaba Gerald <masabagerald@gmail.com>
2016-10-28 14:51:36 +0300

    first commit

---
+++ www/views/events/events.js
@@ -0,0 +1,19 @@
+angular.module('App').controller('EventsCtrl', function ($scope, EventsService) {
+
+  var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+//get the current day using local time
+  var day = new Date().getDay();
+  var index = 0;
+  
+  $scope.events = EventsService.$asArray();
+
+
+
+  $scope.events.$loaded().then(function () {
+    while (day != index && index < 7) {
+      $scope.events.push($scope.events.shift());
+      index++;
+    }
+  });
+});
